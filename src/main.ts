0332f417143e11701c49450aa206680736302ede
tekuno3 <tekuteku777777@gmail.com>
2025-05-04 16:12:53 +0000

    webhookの形式を確認するための処理をちょっと変えた

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { Hono } from '@hono/hono';
 
 const app = new Hono();
-app.get('/ping', (c) => c.json({ text: 'pong!', }));
+app.get('/ping', (c) => c.json({ text: 'pong!' }));
 
 app.post('/webhook-test', (c) => {
-    c.req.json().then(console.log).catch(() => {});
+  c.req.json()
+    .then((v) => { console.log(JSON.stringify(v)) })
+    .catch(() => {});
 
-    return c.notFound();
+  return c.notFound();
 });
 
 export default app;
